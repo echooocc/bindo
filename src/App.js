08666e6f51b37caf4bd9489986055c2c6f17fc93
echooocc <echo2go@gmail.com>
2017-08-07 21:52:50 +0800

    add search function

--- src/App.js
+++ src/App.js
@@ -3,6 +3,27 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [],
+      filterName: ''
+    };
+    this.handleFilterName = this.handleFilterName.bind(this);
+  }
+
+  componentDidMount() {
+    fetch('http://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(users => this.setState(function(){return {users: users}}));
+  }
+
+  handleFilterName(event) {
+    let target = event.currentTarget;
+    console.log(target.value);
+    this.setState(function(prevState){return {filterName: target.value}});
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,12 +31,17 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {this.state.users.map(user =>
+          user.name.toLowerCase().indexOf(this.state.filterName)>=0 ? <div key={user.id}>{user.name}</div> : ''
+        )}
+        <form>
+          <input type="text" value={this.state.filterName} onChange={this.handleFilterName} />
+        </form>
       </div>
     );
   }
 }
+
+
 
 export default App;
